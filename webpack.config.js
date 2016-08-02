587ee3562beeb390619cadc113ad328414622a16
wpcfan <peng.wang@outlook.com>
2016-08-02 18:40:14 +0800

    infrastructure setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,43 @@
-module.exports = {
-    entry: "./src/index.tsx",
-    output: {
-        filename: "./dist/bundle.js",
-    },
+const {resolve} = require('path');
+const validate = require('webpack-validator');
+module.exports = env => {
+    return validate({
+        entry: './src/index.tsx',
+        output: {
+            filename: 'bundle.js',
+            path: resolve(__dirname, 'dist'),
+            pathinfo: !env.prod,
+        },
+        context: resolve(__dirname, 'src'),
+        // Enable sourcemaps for debugging webpack's output.
+        devtool: env.prod ? 'source-map' : 'eval',
+        bail: env.prod,
+        resolve: {
+            // Add '.ts' and '.tsx' as resolvable extensions.
+            extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+        },
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+        module: {
+            loaders: [
+                // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+                { test: /\.tsx?$/, loader: 'ts-loader' }
+            ],
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"]
-    },
+            preLoaders: [
+                { test: /\.js$/, loader: 'babel!eslint', exclude: /node_modules/ },
+                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                { test: /\.js$/, loader: 'source-map-loader' },
+                { test: /\.css$/, loader: 'style!css' },
+            ]
+        },
 
-    module: {
-        loaders: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-            { test: /\.tsx?$/, loader: "ts-loader" }
-        ],
-
-        preLoaders: [
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
-
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+        // When importing a module whose path matches one of the following, just
+        // assume a corresponding global variable exists and use that instead.
+        // This is important because it allows us to avoid bundling all of our
+        // dependencies, which allows browsers to cache those libraries between builds.
+        externals: {
+            react: 'React',
+            'react-dom': 'ReactDOM'
+        },
+    });
 };
