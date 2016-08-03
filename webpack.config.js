58205ea62eb0952c4ca2d007a8b00f36ab1b9a7e
wpcfan <peng.wang@outlook.com>
2016-08-03 19:10:00 +0600

    using plain js instead of typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const {resolve} = require('path');
 const validate = require('webpack-validator');
+const webpack = require('webpack');
 module.exports = env => {
     return validate({
-        entry: './src/index.tsx',
+        entry: './src/client/index.js',
         output: {
             filename: 'bundle.js',
             path: resolve(__dirname, 'dist'),
@@ -13,30 +14,33 @@ module.exports = env => {
         bail: env.prod,
         resolve: {
             // Add '.ts' and '.tsx' as resolvable extensions.
-            extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+            extensions: ['', '.webpack.js', '.web.js', '.jsx', '.js']
         },
-
+        plugins: [
+            new webpack.optimize.OccurrenceOrderPlugin(),
+            new webpack.HotModuleReplacementPlugin()
+        ],
         module: {
             loaders: [
-                // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-                { test: /\.tsx?$/, loader: 'ts-loader' }
+                // All files with a '.js' or '.jsx' extension will be handled by 'ts-loader'.
+                { test: /\.js$/, loader: 'babel', exclude: /node_modules/ },
+                { test: /\.css$/, loader: 'style!css' },
             ],
 
             preLoaders: [
-                { test: /\.js$/, loader: 'babel!eslint', exclude: /node_modules/ },
                 // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-                { test: /\.js$/, loader: 'source-map-loader' },
-                { test: /\.css$/, loader: 'style!css' },
+                { test: /\.js$/, loader: 'source-map-loader' }
             ]
         },
 
+        
         // When importing a module whose path matches one of the following, just
         // assume a corresponding global variable exists and use that instead.
         // This is important because it allows us to avoid bundling all of our
         // dependencies, which allows browsers to cache those libraries between builds.
-        externals: {
-            'react': 'React',
-            'react-dom': 'ReactDOM'
-        },
+        // externals: {
+        //     'react': 'React',
+        //     'react-dom': 'ReactDOM'
+        // }
     });
 };
